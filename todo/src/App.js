58e84b34bc69b981e805e95ebe317f06ff4ffcfe
Joyce <joycesantos8900@gmail.com>
2024-02-24 16:09:17 -0300

    adding function

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,37 +1,113 @@
 import './App.css';
-import { useState,useEffect } from 'react';
-import {BsTrash, BsBookmarkCheck, BsBookmarkCheckFill} from 'react-icons'
+import { useState, useEffect } from 'react';
+import { BsBookmarkCheck, BsBookmarkCheckFill, BsTrash } from 'react-icons/bs';
 
-const API= 'http://localhost:5000'
+
+const API = 'http://localhost:5000';
 
 function App() {
-  const [title,setTitle] = useState('')
-  const [time, setTime]= useState('')
-  const [todos, setTodos] = useState([])
-  const [loading, Setloading] = useState(false);
-  const handleSubmit = (e)=>{
-    e.preventDefault()
-    console.log('Enviou')
-  }
+  const [title, setTitle] = useState('');
+  const [time, setTime] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const todo = {
+      id: Math.random(),
+      title,
+      time,
+      done: false,
+    };
+    await fetch(API + '/todos', {
+      method: 'POST',
+      body: JSON.stringify(todo),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    setTodos((prevState)=>[...prevState, todo]);
+    setTitle('');
+    setTime('');
+  };
+
+  
+
+  useEffect(() => {
+    const loadData = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch(API + '/todos');
+        const data = await res.json();
+        setTodos(data);
+      } catch (err) {
+        console.log(err);
+      }
+      setLoading(false);
+    };
+    loadData();
+  }, []);
+
+  const handleDelete = async (id) => {
+  await fetch(API + '/todos/' + id, {
+    method: 'DELETE',
+  });
+  setTodos((prevState) => prevState.filter((todo) => todo.id !== id));
+};
+
+  if(loading){ 
+    return <p>Carregando..</p>
+  }
   return (
     <div className="App">
-     <div className='todo-header'>
-      <h1>React Todo</h1>
-
-     </div>
-     <div className='form-todo'>
-       <h2>Insera a sua próxima tarefa:</h2>
-       <form onSubmit={handleSubmit}>
-        <input type='submit' value={'Enviar'}></input>
-       </form>
-
-     </div>
+      <div className='todo-header'>
+        <h1>React Todo</h1>
+      </div>
+      <div className='form-todo'>
+        <h2>Insira a sua próxima tarefa:</h2>
+        <form onSubmit={handleSubmit}>
+          <div className='form-control'>
+            <label htmlFor='title'>O que você vai fazer?</label>
+            <input 
+              type='text' 
+              name='title' 
+              placeholder='Título da Tarefa' 
+              onChange={(e) => setTitle(e.target.value)} 
+              value={title || ''}
+              required
+            />
+          </div>
+          <div className='form-control'>
+            <label htmlFor='time'>Duração:</label>
+            <input 
+              type='text' 
+              name='time' 
+              placeholder='Tempo estimado (em horas)' 
+              onChange={(e) => setTime(e.target.value)} 
+              value={time || ''}
+              required
+            />
+          </div>
+          <input type='submit' value='Criar Tarefa' />
+        </form>
+      </div>
       <div className='list-todo'>
-        <p>
-          <h2>Lista de Tarefas</h2>
-          {todos.length === 0 && <p>Não há tarefas!</p>}
-        </p>
+        <h2>Lista de Tarefas:</h2>
+        {todos.length === 0 && <p>Não há tarefas!</p>}
+        {todos.map((todo)=> (
+          <div className='todo' key={todo.id}> 
+            <h3 className={todo.done ? 'todo-done': '' }>{todo.title}</h3>
+            <p>Tempo estimado: {todo.time} horas</p>
+            <div>
+              <span className={!todo.done ? 'todo-icon' : 'todo-icon-done'}>
+                {!todo.done ? <BsBookmarkCheck/> : <BsBookmarkCheckFill/> }
+              </span>
+              
+              <BsTrash onClick={() => handleDelete(todo.id)} /> 
+            </div>
+          </div>
+
+        ))}
       </div>
     </div>
   );
